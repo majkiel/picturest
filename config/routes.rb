864711f0070c21d363298772a1b4a82d3b360846
Krzysiek <kkonopacki@gmail.com>
2020-02-11 14:25:10 +0100

    about and terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get 'about', to: 'home#about'
+  get 'terms', to: 'home#terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
